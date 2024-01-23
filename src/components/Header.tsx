2ee3b9e9e8f648212104208ac8a16e3947626044
karooolis <hello@karolisram.com>
2024-01-23 21:44:33 +0200

    add back theme toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -469,7 +469,7 @@ export function Header() {
               </div>
               <div className="flex justify-end md:flex-1">
                 <div className="pointer-events-auto">
-                  {/* <ThemeToggle /> */}
+                  <ThemeToggle />
                 </div>
               </div>
             </div>
