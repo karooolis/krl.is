b65d04bb3b8da4c99d126b293c84504a530fe5a2
karooolis <hello@karolisram.com>
2024-02-05 11:28:50 +0200

    update get in touch

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,7 @@ import { SimpleLayout } from './SimpleLayout'
 
 export default function Contact() {
   return (
-    <SimpleLayout id="contact" title="Let’s talk about your project">
+    <SimpleLayout id="contact" title="Get in touch">
       <div className="mx-auto mt-8 overflow-hidden rounded-3xl ring-1 ring-gray-900/10">
         <Widget
           id="j9W549x2"
